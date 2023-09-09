6c106f6d7bc73b163005cafb3b6cc7299d3ec430
pthien24 <72865946+pthien24@users.noreply.github.com>
2023-09-09 20:49:49 +0700

    first commit

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('title', $viewdata['title'])
+@section('subtitle', $viewdata['subtitle'])
+@section('content')
+<div class="heading">
+   <h3>{{$viewdata['title']}}</h3>
+   <p><a href="home.html">home </a> <span> / {{$viewdata['subtitle']}}</span></p>
+</div>
+
+<section class="contact">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/contact-img.svg" alt="">
+      </div>
+
+      <form action="" method="post">
+         <h3>tell us something!</h3>
+         <input type="text" name="name" required placeholder="enter your name" maxlength="50" class="box">
+         <input type="number" name="number" required placeholder="enter your number" max="9999999999" min="0" class="box" onkeypress="if(this.value.length == 10) return false;">
+         <input type="email" name="email" required placeholder="enter your email" maxlength="50" class="box">
+         <textarea name="msg" placeholder="enter your message" required class="box" cols="30" rows="10" maxlength="500"></textarea>
+         <input type="submit" value="send message" class="btn" name="send">
+      </form>
+
+   </div>
+
+</section>
+@endsection
